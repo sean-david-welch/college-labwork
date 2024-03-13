508eae14d47caccb649a4b2b239c12cac00fed75
Sean Welch <seanwelch51199@icloud.com>
2024-03-13 10:08:32 +0000

    bootstrapped classes for apps

---
+++ Week7Lab/src/guessing/GuessingApp.java
@@ -0,0 +1,14 @@
+package guessing;
+
+// Question 2
+// Alter the guessing game application so that every time it plays the user is asked do they want to play again.
+// Depending on what the user enters the application should keep going until they wish to stop.
+
+// Bonus: store the user guesses and return them to the user when they finish or win.
+// Note this may necessitate the creation of a large blank array
+
+public  class GuessingApp {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
---
+++ Week7Lab/src/subjects/SubjectsApp.java
@@ -0,0 +1,13 @@
+package subjects;
+
+// Question 1
+// Create an application that asks a user to enter how many subjects they have.
+// The application should then ask the user to enter the results for each subject.
+// Store these results in an array and print them back to the user along with their overall average and grade for each module.
+
+public class SubjectsApp {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
+
