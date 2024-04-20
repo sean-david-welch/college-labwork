052780083eca6141dea37a8fe5f382635057bab0
Sean Welch <seanwelch51199@icloud.com>
2024-04-20 20:42:48 +0100

    some responsivity

--- WebDesignFinal/src/scripts/navbar.js
+++ WebDesignFinal/src/scripts/navbar.js
@@ -1,10 +1,28 @@
-function toggleNavbarTransparency() {
-	const navbar = document.querySelector('.navbar');
-	if (window.scrollY > 50) {
-		navbar.classList.remove('transparent-nav');
-	} else {
-		navbar.classList.add('transparent-nav');
-	}
-}
+// function toggleNavbarTransparency() {
+//     const navbar = document.querySelector('.navbar');
+//     if (window.scrollY > 50) {
+//         navbar.classList.remove('transparent-nav');
+//     } else {
+//         navbar.classList.add('transparent-nav');
+//     }
+// }
 
-window.addEventListener('scroll', toggleNavbarTransparency);
+// if (window.innerWidth > 768) {
+//     window.addEventListener('scroll', toggleNavbarTransparency);
+// }
+
+// jquery version
+$(document).ready(function () {
+	function toggleNavbarTransparency() {
+		if ($(window).scrollTop() > 50) {
+			$('.navbar').removeClass('transparent-nav');
+		} else {
+			$('.navbar').addClass('transparent-nav');
+		}
+	}
+
+	// Check if the viewport width is more than 768px before attaching the event
+	if ($(window).width() > 768) {
+		$(window).scroll(toggleNavbarTransparency);
+	}
+});
