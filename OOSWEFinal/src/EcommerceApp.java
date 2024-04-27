8bde2c12d8a5f82919539b6b930bfaad7a1e7c0f
seanwelch <seanwelch51199@icloud.com>
2024-04-27 09:09:00 +0100

    service layer

--- OOSWEFinal/src/EcommerceApp.java
+++ OOSWEFinal/src/EcommerceApp.java
@@ -1,7 +1,7 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class EcommerceApp {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
             while (true) {
---
+++ OOSWEFinal/src/EcommerceService.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class EcommerceService {
+    private final Scanner scanner;
+
+    public  EcommerceService(Scanner scanner) {
+        this.scanner = scanner;
+    }
+}
